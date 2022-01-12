b3a669e0c5cacc31bc4607488ba64b98d48fde52
Ana Gabriele <gabbanasoares@gmail.com>
2022-01-11 17:09:03 -0800

    Submit Recursive excercises

--- part-1.py
+++ part-1.py
@@ -5,16 +5,56 @@
 
 # factorial
 
+# factorial - number * itself -1 until it reaches 1
+# base case n -1 =1
+# recursive case number * itself - 1
+
+
+def factorial(n):
+    if(n) <0:
+        raise ValueError()
+        
+    if (n) ==0:
+        return 1
+    
+    return  n * factorial(n-1)
 
 
 # reverse
+def reverse(word):
+    
+    if len(word) <= 1:
+        return word
+    
+    first_char = word[:1]
+    remaining_chars = word[1:]
+    return reverse(remaining_chars) + first_char    
+    
+    
+    
 
+print(reverse('hello'))
 
 
 # bunny
-
+def bunny(num):
+    if num == 0:
+        return 0
+    
+    return bunny(num-1) +2
+    
+ 
+    
 
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        print(parens[:1])
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
+    
--- part-2.py
+++ part-2.py
@@ -4,13 +4,54 @@
 # the appropriate comment.
 
 # search
-
+def search(arr, target):
+    if not arr:
+        return False
+    elif arr[0] == target:
+        return True
+        
+    print(arr[1:])
+  
+    return search(arr[1:], target)
+            
+            
+         
 
 
 # is_palindrome
 
+def  is_palindrome(word):
+    
+    reversed_word = reverse(word)
 
+    if word == reversed_word:
+        return True
+    else:
+        return False
+
+
+
+def reverse(word):
+    if len(word) <= 1:
+        return word
+    
+    first_char = word[:1]
+    remaining_chars = word[1:]
+    return reverse(remaining_chars) + first_char 
 
 # digit_match
+
+
+def digit_match(int1, int2):
+
+    if int1 <10 or int2 <10:
+        if int1 % 10 == int2 %10 :
+           return 1
+        else:
+            return 0
+    if int1 % 10 == int2%10:
+        return 1 + digit_match(int1//10, int2//10)
+        
+    return digit_match(int1//10, int2//10)
 
 
